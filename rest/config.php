f990e34ccbdc242c0484447aa0e58c6e65400be2
dzelila <dzelila@tribeos.io>
2020-05-18 12:19:30 +0200

    skeleton

--- rest/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Config{
-  const CONNECTION_STRING = "mysql:host=sql7.freemysqlhosting.net;dbname=sql7341110"; //change this
-  const USERNAME = "sql7341110"; //change this
-  const PASSWORD = "iVHrQt9qb1"; //change this
-}
- ?>

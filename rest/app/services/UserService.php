7af401b2b3d87f8ac1f202279176a0c67dee475d
dzelila <dzelila@tribeos.io>
2020-06-04 10:19:20 +0200

    users

--- rest/app/services/UserService.php
+++ rest/app/services/UserService.php
@@ -9,15 +9,27 @@ class UserService {
 
     public function login($user){
       $db_user = $this->get_user_by_email($user['user_email']);
-
+      $is_admin = false;
       if($db_user){
         //UPDATE PASSWORD VERIFICATION
         if($db_user['password'] == $user['psword']){
+          if($db_user['type'] == 'employee'){
+            $employee_dao = new EmployeeDao();
+            $db_user = $employee_dao->get_by_id($db_user['id']);
+          } else if($db_user['type'] == 'customer'){
+            $customer_dao = new CustomerDao();
+            $db_user = $customer_dao->get_by_id($db_user['id']);
+          } else if($db_user['type'] == 'admin'){
+            $admin_dao = new AdminDao();
+            $db_user = $admin_dao->get_by_id($db_user['id']);
+            $is_admin = true;
+          }
+
           $token_data = [
             'id' => $db_user['id'],
             'email' => $db_user['email'],
             'name' => $db_user['name'],
-            'is_admin' => false
+            'is_admin' => $is_admin
           ];
 
           $jwt = Auth::encode_jwt($token_data);
@@ -33,6 +45,12 @@ class UserService {
     public function get_user_by_email($user_email){
       $user = $this->user_dao->get_user_by_email($user_email);
       return $user;
+    }
+
+    public function add_user($user){
+      $user_id = $this->user_dao->add($user);
+
+      return $user_id;
     }
 
 
---
+++ rest/config/config.php
@@ -0,0 +1,60 @@
+<?php
+define('LOG_FILE', __DIR__ . '/../logs/debug.log');
+define('DEBUG', true);
+
+/* Environment */
+define('VERSION', '1.0');
+define('ENV', 'DEV'); // PROD for https, DEV for local running
+
+define('DOCS_FOLDER', 'docs'); // relative path from the project root
+/* Folders/files containing OpenAPI annotations. */
+define('DOCS_ANNOTATION_LOCATIONS', [
+    __DIR__ . '/../app/models/',
+    __DIR__ . '/../app/routes/',
+    __DIR__ . '/../app/dao/',
+    __DIR__ . '/../docs/doc_setup.php'
+]);
+
+/* Project definitions */
+if (explode(' ', php_uname('s'))[0] === 'Windows') {
+    $slash = "\\";
+} else {
+    $slash = '/';
+}
+
+/* Define the API base path */
+if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') && ENV === 'PROD') {
+    $base_path = 'https://' . $_SERVER['HTTP_HOST'] . '/';
+} else {
+    $base_path = 'http://localhost/canteenms/rest'; // $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $slash . explode($slash, $_SERVER['REQUEST_URI'])[1];
+}
+define('API_BASE_PATH', $base_path);
+define('SERVER_ROOT', __DIR__ . '/..');
+
+define('AUTH_HEADER_NAME', 'authorization'); // name of the authorization header to be used
+define('SERVER_DESCRIPTION', 'Canteen.'); // description of the host server
+define('PROJECT_TITLE', 'Canteen'); // project title
+define('PROJECT_DESCRIPTION', ''); // project description
+define('PROJECT_VERSION', '1.0'); // project version
+define('PROJECT_DOCS_TITLE', 'Canteen'); // title of the HTML page for the generated documentation
+
+/* Author/team definitions */
+define('AUTHOR_NAME', 'Sajra Agić');
+define('AUTHOR_EMAIL', 'sajra.agic@stu.ibu.edu.ba');
+define('AUTHOR_URL', '');
+
+// DB settings
+/*define('CONNECTION_STRING', "mysql:host=sql7.freemysqlhosting.net;dbname=sql7341110");
+define('DB_USER', "sql7341110");
+define('DB_PASS', "iVHrQt9qb1");*/
+
+// mongodb settings
+define('CONNECTION_STRING', "mysql:host=remotemysql.com;dbname=tEYQ0vL2Rn");
+define('DB_USER', "tEYQ0vL2Rn");
+define('DB_PASS', "Xwqg1jEEB0");
+
+
+
+// jwt settings
+define('JWT_KEY', "canteen-token");
+define('JWT_TOKEN_VALIDITY', "+10 days"); // example; +5minutes, +1 hour
